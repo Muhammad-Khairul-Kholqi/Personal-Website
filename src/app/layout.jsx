100db76e72da5d4a8390d6987078684bd8b02e97
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2025-07-31 10:55:08 +0700

    add title & logo tab

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,6 +12,11 @@ const sora = Sora({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={sora.variable}> 
+      <head>
+        <title>Khairul Kholqi | Personal Website</title>
+        <link rel="icon" href="https://drive.google.com/uc?export=view&id=1R8Jp_g8IaBcS635L91pQooHM7Ns7j7xh" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={`${sora.className} font-sora overflow-hidden`}> 
         <MobileHeader />
         <div className="flex justify-center p-5 pt-20 lg:pt-5 h-screen">
